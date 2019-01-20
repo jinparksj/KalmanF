207a4ba4c8f71f94e113e51067859729c27a7559
jinparksj <jinparksj@gmail.com>
2019-01-20 00:35:00 -0800

    ongoing random constant estimate in offline

--- test1_random_const_estimate.cpp
+++
@@ -1,4 +0,0 @@
-//
-// Created by jin on 1/19/19.
-//
-
---
+++ test1_random_const_estimate_offline.cpp
@@ -0,0 +1,17 @@
+//
+// Created by jin on 1/19/19.
+//
+
+#include <opencv2/opencv.hpp>
+#include <time.h>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+    //Greg Welch and Gary Bishop, "An Introduction to the Kalman Filter", 2006
+    //Estimating a Random Constant: Offline
+
+
+}
