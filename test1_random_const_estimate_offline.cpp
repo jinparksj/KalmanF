edb267b8b262ad532a6dfde933c137b71e3ad49b
jinparksj <jinparksj@gmail.com>
2019-01-20 01:13:24 -0800

    ongoing random constant estimate in offline

--- test1_random_const_estimate_offline.cpp
+++ test1_random_const_estimate_offline.cpp
@@ -13,5 +13,20 @@ int main()
     //Greg Welch and Gary Bishop, "An Introduction to the Kalman Filter", 2006
     //Estimating a Random Constant: Offline
 
+    theRNG().state = time(NULL);
+    //random number - normal/Gaussian distribution
+    //default random generator with state of time -> every execution has different random number
+
+    int t, count = 100;
+    double x = -0.377; //truth value
+
+    ///////// Kalman Filter /////////
+    double Q = 1e-5; //process variance
+    double R = 0.0001; //estimate of measurement variance
+
+    Scalar stddevR = Scalar::all(0.1);
+    vector<float> measurement_k(count);
+
+
 
 }
